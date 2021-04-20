e668880b78bae2ad3f8edef8d851baa5bbd71951
Felipe Ghidini <felipe.ghidini@gft.com>
2021-04-19 22:22:50 -0300

    Melhorando a funcionalidade

--- scripts.js
+++ scripts.js
@@ -6,3 +6,77 @@ const modal = {
         document.querySelector('.modal-overlay').classList.remove('active');
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021'
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021'
+    }, 
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021'
+    }, 
+    {
+        id: 4,
+        description: 'App',
+        amount: 20000,
+        date: '23/01/2021'
+    }
+    ]
+
+const Transaction = {
+    incomes() {
+        //somar as entradas
+    },
+    expensive() {
+        //somar as saídas
+    },
+    total() {
+
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        console.log(tr.innerHTML)
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense" 
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+        `
+        return html
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+    }
+}
+
+ transactions.forEach(function(transaction){
+     DOM.addTransaction(transaction)
+ })
